08700ce318821d23f171eaec8431773ee056de12
hjpotter <hjpotter2@gmail.com>
2024-03-24 22:46:34 +0900

    aside-menu active

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ const main_header = document.querySelector('.contect .text-description h1');
 const second_header = document.querySelector('.contect .text-description h2');
 const paragraph = document.querySelector('.contect  .text-description p');
 const text_link = document.querySelector('.contect  .text-description a');
+const menu_button = document.querySelectorAll('.aside-menu a');
 
 active_video[0].classList.remove(ACTIVE);
 active_video[0].classList.add(ACTIVE);
@@ -21,4 +22,12 @@ text_link.innerHTML = 'Explore';
 menu_logo.addEventListener('click', () => {
   menu_logo.classList.toggle(ACTIVE);
   main_structrue.classList.toggle(ACTIVE);
+});
+menu_button.forEach((button, index) => {
+  button.addEventListener('click', () => {
+    active_video.forEach((video) => {
+      video.classList.remove(ACTIVE);
+    });
+    active_video[index].classList.add(ACTIVE);
+  });
 });
