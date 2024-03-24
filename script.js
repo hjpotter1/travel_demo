aa558b1d91ee69b25151d81ffc51cef3e1681ba1
hjpotter <hjpotter2@gmail.com>
2024-03-24 21:19:45 +0900

    main_structrue active

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ const active_video = document.querySelectorAll(
   '.main-structure .contect video '
 );
 
+const menu_logo = document.querySelector('header .menu-logo');
+const main_structrue = document.querySelector('.main-structure');
 const main_header = document.querySelector('.contect .text-description h1');
 const second_header = document.querySelector('.contect .text-description h2');
-const paragraph_header = document.querySelector(
-  '.contect  .text-description p'
-);
+const paragraph = document.querySelector('.contect  .text-description p');
 const text_link = document.querySelector('.contect  .text-description a');
 
 active_video[0].classList.remove(ACTIVE);
@@ -15,5 +15,10 @@ active_video[0].classList.add(ACTIVE);
 
 main_header.innerHTML = CONTENT_INFO.island.h1;
 second_header.innerHTML = CONTENT_INFO.island.h2;
-paragraph_header.innerHTML = CONTENT_INFO.island.p;
+paragraph.innerHTML = CONTENT_INFO.island.p;
 text_link.innerHTML = 'Explore';
+
+menu_logo.addEventListener('click', () => {
+  menu_logo.classList.toggle(ACTIVE);
+  main_structrue.classList.toggle(ACTIVE);
+});
