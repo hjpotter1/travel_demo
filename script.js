1b9ed7bff54cfb1810ac8e6f39d2ceaf3b549c62
hjpotter <hjpotter2@gmail.com>
2024-03-27 01:57:47 +0900

    bug fix

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const ACTIVE = 'active';
 const active_video = document.querySelectorAll(
   '.main-structure .contect video '
 );
-const active_video_overlay = document.querySelectorAll(
+const active_video_overlay = document.querySelector(
   '.main-structure .contect .video-overlay '
 );
 
@@ -27,11 +27,49 @@ menu_logo.addEventListener('click', () => {
   menu_logo.classList.toggle(ACTIVE);
   main_structrue.classList.toggle(ACTIVE);
 });
+
 menu_button.forEach((button, index) => {
   button.addEventListener('click', () => {
     active_video.forEach((video) => {
       video.classList.remove(ACTIVE);
     });
     active_video[index].classList.add(ACTIVE);
+    modify_content(index);
   });
 });
+function modify_content(index) {
+  switch (index) {
+    case 0:
+      main_header.innerHTML = CONTENT_INFO.island.h1;
+      second_header.innerHTML = CONTENT_INFO.island.h2;
+      paragraph.innerHTML = CONTENT_INFO.island.p;
+      active_video_overlay.className = 'video-overlay video-overlay-island';
+      break;
+    case 1:
+      main_header.innerHTML = CONTENT_INFO.spring.h1;
+      second_header.innerHTML = CONTENT_INFO.spring.h2;
+      paragraph.innerHTML = CONTENT_INFO.spring.p;
+      active_video_overlay.className = 'video-overlay video-overlay-spring';
+      break;
+    case 2:
+      main_header.innerHTML = CONTENT_INFO.summer.h1;
+      second_header.innerHTML = CONTENT_INFO.summer.h2;
+      paragraph.innerHTML = CONTENT_INFO.summer.p;
+      active_video_overlay.className = 'video-overlay video-overlay-summer';
+      break;
+    case 3:
+      main_header.innerHTML = CONTENT_INFO.autumn.h1;
+      second_header.innerHTML = CONTENT_INFO.autumn.h2;
+      paragraph.innerHTML = CONTENT_INFO.autumn.p;
+      active_video_overlay.className = 'video-overlay video-overlay-autumn';
+      break;
+    case 4:
+      main_header.innerHTML = CONTENT_INFO.winter.h1;
+      second_header.innerHTML = CONTENT_INFO.winter.h2;
+      paragraph.innerHTML = CONTENT_INFO.winter.p;
+      active_video_overlay.className = 'video-overlay video-overlay-winter';
+      break;
+    default:
+      break;
+  }
+}
--- src/data/content.js
+++ src/data/content.js
@@ -11,17 +11,17 @@ const CONTENT_INFO = {
   },
   summer: {
     h1: 'summer',
-    h2: 'Feel the new vitality',
+    h2: 'ENJOY PASSION TIME',
     p: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vero inventore sint fugit animi earum quas necessitatibus, sunt sit, quibusdam at quidem qui rem nostrum dolores exercitationem dicta voluptas neque? Consequatur.',
   },
   autumn: {
     h1: 'Autumn',
-    h2: 'Feel the new vitality',
+    h2: 'EXPRERIENCE WITHOUT LIMITS',
     p: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vero inventore sint fugit animi earum quas necessitatibus, sunt sit, quibusdam at quidem qui rem nostrum dolores exercitationem dicta voluptas neque? Consequatur.',
   },
   winter: {
     h1: 'Winter',
-    h2: 'Feel the new vitality',
+    h2: 'ENJOY PEACE AND FREEDOM',
     p: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vero inventore sint fugit animi earum quas necessitatibus, sunt sit, quibusdam at quidem qui rem nostrum dolores exercitationem dicta voluptas neque? Consequatur.',
   },
 };
